5a6d2b3615ac9b59035ca7c8d9012a712eb0a2d3
HasicAdnan <adnan.hasic@hotmail.com>
2023-12-13 13:20:27 +0100

    changed package.json

--- promises/index.js
+++ promises/index.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 import readline from 'readline';
 import readFile from 'node:fs/promises';
 
-/*
+
 const fs = require('fs/promises');
 
 readFile('./package.json', 'utf8')
@@ -18,4 +18,3 @@ console.error('Error reading file', err);
 console.log('After read or error');
 });
 console.log('Before file was loaded');
-*/
