9f837d8f3713e9ce46f4a509baf9faa16aac5484
HasicAdnan <adnan.hasic@hotmail.com>
2023-12-13 13:30:23 +0100

    changed index.js

--- promises/index.js
+++ promises/index.js
@@ -1,11 +1,14 @@
-import fs from 'fs';
 import chalk from 'chalk';
 import moment from 'moment';
 import readline from 'readline';
-import readFile from 'node:fs/promises';
+import readFile from 'fs/promises';
 
 
 const fs = require('fs/promises');
+
+async function main() {
+    await fs.writeFile('test.txt', 'hello');
+  }
 
 readFile('./package.json', 'utf8')
 .then((data) => {
@@ -18,3 +21,16 @@ console.error('Error reading file', err);
 console.log('After read or error');
 });
 console.log('Before file was loaded');
+
+async function addProduct() {
+    const number = await prompt(chalk.bgGreenBright.bold('Geben Sie Produktnummer ein: '));
+    const name = await prompt(chalk.bgGreen.bold('Geben Sie Produktname ein: '));
+    const price = await prompt(chalk.bgGreenBright.bold('Geben Sie den Preis des Produkts ein: '));
+    
+  
+    const newProduct = { number, name, price };
+    data.push(newProduct);
+    console.log('Das Produkt wurde eingegeben:', newProduct);
+    askUser();
+  }
+  
